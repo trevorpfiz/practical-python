efce9e420188efec2bcb8ae72fa59403c5e5a844
ElektrikSpark <elektrikspark@gmail.com>
2024-03-01 01:14:33 -0600

    finished chapter 1

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+month = 0
+
+while principal > 0:
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        principal = principal * (1 + rate / 12) - (payment + extra_payment)
+        total_paid = total_paid + payment + extra_payment
+    elif principal < payment:
+        total_paid = total_paid + principal
+        principal = 0
+    else:
+        principal = principal * (1 + rate / 12) - payment
+        total_paid = total_paid + payment
+
+    month += 1
+
+    print(month, total_paid, principal)
+
+print(f"Total paid is ${total_paid:,.2f}")
+print("Months", month)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import sys
+
+
+def portfolio_cost(filename):
+    ...
+    # Your code here
+    ...
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+    print("Filename:", filename)
+else:
+    filename = "Data/portfolio.csv"
+    print("argv" + sys.argv[0])
+
+cost = portfolio_cost(filename)
+print("Total cost:", cost)
